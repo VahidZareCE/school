73e738d900799b4f5d6830ec54e9714c9ba3918a
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-07 17:40:30 +0330

    add student admin

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from account.models import User, ProfileTeacher
+from account.models import User, ProfileTeacher, ProfileStudent
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(ProfileTeacher)
+admin.site.register(ProfileStudent)
