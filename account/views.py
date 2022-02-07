c67886f6081501ed0e4cbb48a66ec14acd6d71e3
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-07 17:43:11 +0330

    add view for student

--- account/views.py
+++ account/views.py
@@ -1,17 +1,13 @@
-
-from django.shortcuts import render
-
-from rest_framework.generics import CreateAPIView, ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from account.models import ProfileTeacher, User
-from account.serializers import TeacherSerializer
+from account.serializers import TeacherSerializer, StudentSerializer, TokenSerializer
+from account.models import ProfileTeacher, ProfileStudent
 
-# Create your views here.
-
-class UserList(ListCreateAPIView):
-    # permission_classes = (IsAuthenticatedOrWriteOnly,)
+class ListCreateTeacherView(ListCreateAPIView):
     queryset = ProfileTeacher.objects.all()
     serializer_class = TeacherSerializer
 
@@ -21,3 +17,19 @@ class UserList(ListCreateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ListCreateStudentView(ListCreateAPIView):
+    queryset = ProfileStudent.objects.all()
+    serializer_class = StudentSerializer
+
+    def post(self, request, format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = TokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
