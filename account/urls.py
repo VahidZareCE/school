7c8b877cb43a24920f55f826d0ca8fdfbab0d218
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-09 21:13:10 +0330

    add url change password and edit profile

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from account.views import ListCreateTeacherView, ListCreateStudentView, CreateTokenView
+from account.views import ListCreateTeacherView, ListCreateStudentView, CreateTokenView, UpdateProfile, ChangePassword
 
 urlpatterns = [
     path('register/teacher/', ListCreateTeacherView.as_view()),
     path('register/student/', ListCreateStudentView.as_view()),
-    path('token/', CreateTokenView.as_view())
+    path('update/profile/', UpdateProfile.as_view()),
+    path('token/', CreateTokenView.as_view()),
+
+    path('account/changpassword/', ChangePassword.as_view())
 ]
