3d8a7a238fe5d9658ab10a9be05b62eb77c609a6
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-09 21:13:55 +0330

    add change password view and add edit profile view

--- account/views.py
+++ account/views.py
@@ -1,11 +1,20 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
-from account.serializers import TeacherSerializer, StudentSerializer, TokenSerializer
-from account.models import ProfileTeacher, ProfileStudent
+from account.serializers import (
+                                    TeacherSerializer, 
+                                    StudentSerializer, 
+                                    TokenSerializer, 
+                                    UpdateStudentSerializer, 
+                                    UpdateTeacherSerializer, 
+                                    ChangePasswordSerializer
+                                )
+from account.models import User, ProfileTeacher, ProfileStudent
 
 class ListCreateTeacherView(ListCreateAPIView):
     queryset = ProfileTeacher.objects.all()
@@ -33,3 +42,47 @@ class ListCreateStudentView(ListCreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = TokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UpdateProfile(RetrieveUpdateAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.request.user.is_teacher:
+            return UpdateTeacherSerializer
+        if self.request.user.is_student:
+            return UpdateStudentSerializer
+
+    def get_object(self):
+        if self.request.user.is_teacher:
+            return ProfileTeacher.objects.get(user=self.request.user)
+        if self.request.user.is_student:
+            return ProfileStudent.objects.get(user=self.request.user)
+
+class ChangePassword(UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model=User
+    authentication_classes=(TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+    
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not self.object.check_password(serializer.data.get('old_password')):
+                return Response({'old_password':['Wrong password.']}, status=status.HTTP_400_BAD_REQUEST)
+            self.object.set_password(serializer.data.get('new_password'))
+            self.object.save()
+            response = {
+                'status':'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password update successfully.',
+                'data':[]
+            }
+            return Response(response)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
