ae884a53526ba7e71ed736d0f8305d74f4aaadfc
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-12 11:05:33 +0330

    edit views news

--- news/views.py
+++ news/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
-from news.serializers import NewsSerializer
+from news.serializers import NewsSerializer, NewsDetailSerializer
 from news.models import News
 from news.permissions import IsStudent, IsTeacher
 
@@ -20,9 +20,13 @@ class ListCreateNews(ListCreateAPIView):
         user = ProfileTeacher.objects.get(user=self.request.user)
         serializer.save(origin=user)
 
+    def get_queryset(self):
+        user = ProfileTeacher.objects.get(user=self.request.user)
+        return News.objects.filter(origin=user)
+
 class UpdateDeleteNews(RetrieveUpdateDestroyAPIView):
     queryset = News.objects.all()
-    serializer_class = NewsSerializer
+    serializer_class = NewsDetailSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsTeacher)
 
@@ -30,15 +34,16 @@ class UpdateDeleteNews(RetrieveUpdateDestroyAPIView):
         user = ProfileTeacher.objects.get(user=self.request.user)
         serializer.save(origin=user)
 
+    def get_queryset(self):
+        user = ProfileTeacher.objects.get(user=self.request.user)
+        return News.objects.filter(origin=user)
+
 class ListNewsStudent(ListAPIView):
     queryset = News.objects.all()
-    serializer_class = NewsSerializer
+    serializer_class = NewsDetailSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsStudent)
 
     def get_queryset(self):
         user = ProfileStudent.objects.get(user=self.request.user)
-        news = News.objects.values_list('destination__national_code')
-        for new in news:
-            if user.national_code in new:
-                return News.objects.filter(status='published')
+        return News.objects.filter(destination__national_code=user.national_code)
