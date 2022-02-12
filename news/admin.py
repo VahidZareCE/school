72387d74666e11f032b6269081fc70f5e64aa00f
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-12 11:03:20 +0330

    create admin news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from news.models import News
 # Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'topic', 'pub_date', 'exp_date', 'status')
+    list_filter = ('origin', 'status')
+    search_fields = ('topic', 'origin', 'status')
+    ordering = ('pub_date',)
+
+admin.site.register(News, NewsAdmin)
