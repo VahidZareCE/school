691c4a4c52993f316451595e7196565f03f820fa
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-13 20:04:49 +0330

    add filter to views

--- course/views.py
+++ course/views.py
@@ -1,6 +1,7 @@
 from urllib import request
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 
 from course.models import Exercise, Answer
 from course.serializers import ExerciseSerializer, AnswerSerializer, AnswerDetailSerializer, AnswerDetailTeacherSerializer
@@ -16,6 +17,9 @@ class ListCreateExerciesView(ListCreateAPIView):
     serializer_class = ExerciseSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsTeacher)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['topic', 'status', 'destination__national_code', 'destination__user__first_name', 'destination__user__last_name']
+    ordering_fields = ['exp_answer_date']
 
     def get_queryset(self):
         return Exercise.objects.filter(origin__user=self.request.user)
@@ -42,6 +46,9 @@ class ListCreateAnswerView(ListCreateAPIView):
     serializer_class = AnswerSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsStudent)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['exercise__topic', 'exercise__origin__user__first_name', 'exercise__origin__user__last_name', 'datesend']
+    ordering_fields = ['datesend']
 
     def perform_create(self, serializer):
         user = ProfileStudent.objects.get(user=self.request.user)
@@ -70,6 +77,9 @@ class ListAnswarTeacherView(ListAPIView):
     serializer_class = AnswerDetailSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsTeacher)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['exercise__topic', 'student__national_code', 'datesend']
+    ordering_fields = ['datesend']
 
     def get_queryset(self):
         return Answer.objects.filter(exercise__origin__user=self.request.user)
