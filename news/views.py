9c9f1a51753050811dbe980060c7dfb09ecbc835
VahidZareCE <vahid.zare.ce@gmail.com>
2022-02-13 20:06:07 +0330

    add filter to views

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from urllib import request
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from news.serializers import NewsSerializer, NewsDetailSerializer
 from news.models import News
@@ -15,6 +16,9 @@ class ListCreateNews(ListCreateAPIView):
     serializer_class = NewsSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsTeacher)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['destination__national_code', 'topic', 'status']
+    ordering_fields = ['pub_date', 'exp_date']
 
     def perform_create(self, serializer):
         user = ProfileTeacher.objects.get(user=self.request.user)
@@ -43,7 +47,10 @@ class ListNewsStudent(ListAPIView):
     serializer_class = NewsDetailSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, IsStudent)
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['origin__user__first_name', 'origin__user__last_name', 'topic']
+    ordering_fields = ['pub_date', 'exp_date']
 
     def get_queryset(self):
         user = ProfileStudent.objects.get(user=self.request.user)
-        return News.objects.filter(destination__national_code=user.national_code)
+        return News.objects.filter(destination__national_code=user.national_code).filter(status='published')
